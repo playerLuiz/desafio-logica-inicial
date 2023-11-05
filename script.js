647c84e24138a4708c9292482a2ecb97539b1563
playerLuiz <luizmachine123@gmail.com>
2023-11-05 18:12:07 -0300

    desafio concluido v1

---
+++ script.js
@@ -0,0 +1,31 @@
+// Desafio Classificador de nível de Herói
+
+let nomeHeroi = "Luiz"
+let xpHeroi = 9.000
+let elo1 = "Diamante"
+
+if(xpHeroi <= 1.000){
+    //console.log("Ferro")
+    console.log(nomeHeroi + " está no elo Ferro ")
+}else if(xpHeroi >= 1.001 && xpHeroi <= 2.000){
+    //console.log("Bronze")
+    console.log(nomeHeroi + " está no elo Bronze ")
+}else if(xpHeroi >= 2.001 && xpHeroi <= 5.000){
+    //console.log("Prata")
+    console.log(nomeHeroi + " está no elo Prata ")
+}else if(xpHeroi >= 6.001 && xpHeroi <= 7.000){
+    //console.log("Ouro")
+    console.log(nomeHeroi + " está no elo Ouro ")
+}else if(xpHeroi >= 7.001 && xpHeroi <= 8.000){
+    //console.log("Platina")
+    console.log(nomeHeroi + " está no elo Platina ")
+}else if(xpHeroi >= 8.001 && xpHeroi <= 9.000){
+    //console.log("Diamante")
+    console.log(nomeHeroi + " está no elo Diamante ")
+}else if(xpHeroi >= 9.001 && xpHeroi <= 10.000){
+    //console.log("Imotal")
+    console.log(nomeHeroi + " está no elo Imortal ")
+}else if(xpHeroi >= 10.001 ){
+    //console.log("Radiante")
+    console.log(nomeHeroi + " está no elo Radiante ")
+}
